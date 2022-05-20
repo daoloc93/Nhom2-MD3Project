04aecdc134b594d0725330a29b7b5fd06549570d
Loc <daoloc93@gmail.com>
2022-05-20 09:01:54 +0700

    create project

---
+++ src/main/java/com/example/nhom2projectmd3/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.nhom2projectmd3;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
