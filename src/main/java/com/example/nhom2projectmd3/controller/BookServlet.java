df59de5b4a19722e041f5b99af945621a1801530
locdao <daoloc93@gmail.com>
2022-05-21 17:00:06 +0700

    cap nhat BookDAO, sua model Book cho category va author ve String

--- src/main/java/com/example/nhom2projectmd3/controller/BookServlet.java
+++ src/main/java/com/example/nhom2projectmd3/controller/BookServlet.java
@@ -1,2 +1,91 @@
-package com.example.nhom2projectmd3.controller;public class BookServlet {
+package com.example.nhom2projectmd3.controller;
+
+import com.example.nhom2projectmd3.dao.book.BookDAO;
+import com.example.nhom2projectmd3.model.Book;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet(name = "BookServlet", urlPatterns = "/books")
+public class BookServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private BookDAO bookDAO;
+
+    public void init() {
+        bookDAO = new BookDAO();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        try {
+            switch (action) {
+//                case "create":
+//                    insertBook(request, response);
+//                    break;
+//                case "edit":
+//                    updateBook(request, response);
+//                    break;
+//                case "findUser":
+//                    foundedBook(request, response);
+//                    break;
+                default:
+                    listBook(request, response);
+                    break;
+            }
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+
+        try {
+            switch (action) {
+//                case "create":
+//                    showNewForm(request, response);
+//                    break;
+//                case "edit":
+//                    showEditForm(request, response);
+//                    break;
+//                case "delete":
+//                    deleteUser(request, response);
+//                    break;
+//                case "findUser":
+//                    showFindForm(request, response);
+//                    break;
+//                case "test-without-tran":
+//                    testWithoutTran(request, response);
+//                    break;
+                default:
+                    listBook(request, response);
+                    break;
+            }
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    private void listBook(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException {
+        List<Book> listBook = bookDAO.selectAllBooks();
+        request.setAttribute("listBook", listBook);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("list.jsp");
+        dispatcher.forward(request, response);
+    }
 }
--- src/main/webapp/list.jsp
+++ src/main/webapp/list.jsp
@@ -14,24 +14,27 @@
 </center>
 <div align="center">
     <table border="1" cellpadding="5">
-        <caption><h2>List of Users</h2></caption>
+        <caption><h2>List of Books</h2></caption>
         <tr>
             <th>ID</th>
             <th>Name</th>
-            <th>Email</th>
-            <th>Country</th>
-            <th>Actions</th>
+            <th>Category</th>
+            <th>Author</th>
+            <th>Price</th>
+            <th>Image</th>
+            <th>Release Date</th>
+            <th>Note</th>
         </tr>
-        <c:forEach var="user" items="${listUser}">
+        <c:forEach var="book" items="${listBook}">
             <tr>
-                <td><c:out value="${user.id}"/></td>
-                <td><c:out value="${user.name}"/></td>
-                <td><c:out value="${user.email}"/></td>
-                <td><c:out value="${user.country}"/></td>
-                <td>
-                    <a href="/users?action=edit&id=${user.id}">Edit</a>
-                    <a href="/users?action=delete&id=${user.id}">Delete</a>
-                </td>
+                <td><c:out value="${book.id}"/></td>
+                <td><c:out value="${book.name}"/></td>
+                <td><c:out value="${book.category}"/></td>
+                <td><c:out value="${book.author}"/></td>
+                <td><c:out value="${book.price}"/></td>
+                <td><c:out value="${book.image}"/></td>
+                <td><c:out value="${book.releaseDate}"/></td>
+                <td><c:out value="${book.note}"/></td>
             </tr>
         </c:forEach>
     </table>
